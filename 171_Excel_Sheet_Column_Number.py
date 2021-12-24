3646c7d3172431e82c60643b64ccac04350b6ea7
Shufan Lu <58699794+shufan-mct@users.noreply.github.com>
2021-12-23 23:48:31 -0500

    Create 171_Excel_Sheet_Column_Number.py

---
+++ 171_Excel_Sheet_Column_Number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        num = 0
+        for char in columnTitle:
+            num = num * 26 + ord(char) - ord('A') + 1
+        return num
