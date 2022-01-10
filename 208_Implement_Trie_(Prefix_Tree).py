23def1b6420699a67fe81662f888281e0c83aefa
Shufan Lu <58699794+shufan-mct@users.noreply.github.com>
2022-01-09 23:05:37 -0500

    Create 208_Implement_Trie_(Prefix_Tree).py

---
+++ 208_Implement_Trie_(Prefix_Tree).py
@@ -0,0 +1,36 @@
+class TrieNode:
+    
+    def __init__(self):
+        self.word = False
+        self.children = {}
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()    
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                return False
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                return False
+        return True
