d461cb26150816e2489a54f12746dae42711f7fe
Shufan Lu <58699794+shufan-mct@users.noreply.github.com>
2022-01-21 01:13:51 -0500

    Update 134_Gas_Station.py

--- 134_Gas_Station.py
+++ 134_Gas_Station.py
@@ -1,21 +1,17 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         n = len(gas)
-        gain = []
+        prevGain = 0
+        minGain = float('inf')
+        minGainIdx = 0
+        
         for i in range(n):
-            gain.append(gas[i] - cost[i])
-        
-        prevSum = 0
-        
-        end = n - 1
-        curr = 0
-        while curr <= end:
-            prevSum += gain[curr]
-            while prevSum < 0:
-                prevSum += gain[end]
-                end -= 1
-                if end < curr:
-                    return -1
-            curr += 1
-            
-        return (end + 1) % n
+            prevGain += gas[i] - cost[i]
+            if prevGain < minGain:
+                minGain = prevGain
+                minGainIdx  = i
+                
+        if prevGain < 0:
+            return -1
+        else:
+            return (minGainIdx + 1) % n
