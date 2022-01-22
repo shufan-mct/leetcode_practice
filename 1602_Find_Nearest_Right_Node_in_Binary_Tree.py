3dd9a59d9f865e40188b2874be89b200c67cabd1
Shufan Lu <58699794+shufan-mct@users.noreply.github.com>
2022-01-22 02:40:54 -0500

    Update 1602_Find_Nearest_Right_Node_in_Binary_Tree.py

--- 1602_Find_Nearest_Right_Node_in_Binary_Tree.py
+++ 1602_Find_Nearest_Right_Node_in_Binary_Tree.py
@@ -4,17 +4,22 @@ class Solution:
     def findNearestRightNode(self, root: TreeNode, u: TreeNode) -> Optional[TreeNode]:
         q = Queue()
         q.put(root)
-        while not q.empty():
+        
+        while not q.empty():            
             levelLen = q.qsize()
+            
             for i in range(levelLen):
                 node = q.get()
+                
                 if node == u:
                     if i < levelLen - 1:
                         return q.get()
                     else:
                         return None
+                    
                 if node.left:
                     q.put(node.left)
                 if node.right:
                     q.put(node.right)
+                    
         return None
