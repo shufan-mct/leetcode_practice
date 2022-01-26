504480bea7984f62e2e693c5908fb92004934a82
Shufan Lu <58699794+shufan-mct@users.noreply.github.com>
2022-01-25 23:59:58 -0500

    Create 1470_Shuffle_the_Array.cs

---
+++ 1470_Shuffle_the_Array.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] Shuffle(int[] nums, int n) {
+        int[] shuffled = new int[2 * n];
+        
+        for(int i = 0; i < n; i ++){
+            shuffled[2 * i] = nums[i];
+            shuffled[2 * i + 1] = nums[n + i];
+        }
+        
+        return shuffled;
+    }
+}
