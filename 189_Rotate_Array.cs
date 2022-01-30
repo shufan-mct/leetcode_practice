e7bd29a3faa7f66736c110b1034faf3ae606ce0f
Shufan Lu <58699794+shufan-mct@users.noreply.github.com>
2022-01-30 01:21:10 -0500

    Create 189_Rotate_Array.cs

---
+++ 189_Rotate_Array.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public void Rotate(int[] nums, int k) {
+        int n = nums.Length;
+        k %= n;
+        Queue<int> savedNums = new Queue<int>();
+        for (int i = n - k; i < n; i++){
+            savedNums.Enqueue(nums[i]);
+        }
+        for (int i = 0; i < n; i++){
+            savedNums.Enqueue(nums[i]);
+            nums[i] = savedNums.Dequeue();
+        }
+    }
+}
